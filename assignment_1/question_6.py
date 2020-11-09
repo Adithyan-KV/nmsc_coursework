89f120d37f5077df6faca41ebade57a0af2e487c
adithyan-kv <adithyankv007@gmail.com>
2020-11-10 01:21:40 +0530

    Question 6 done.
     -Added info messages
     -I guess the code is right.

--- assignment_1/question_6.py
+++ assignment_1/question_6.py
@@ -1,26 +1,38 @@
 def main():
+    # defining the functions for iteratively calculating terms of p
     f_1 = lambda p: p * (1 + (7 - p**5) / (p**2))**3
     f_2 = lambda p: p - ((p**5 - 7) / (p**2))**3
     f_3 = lambda p: p - ((p**5 - 7) / (5 * p**4))
     f_4 = lambda p: p - ((p**5 - 7) / (12))
+
+    # run all functions till convergence
     functions = [f_1, f_2, f_3, f_4]
     for function in functions:
-        try:
-            iterate_till_convergence(function)
-        except:
-            print("fucking shit not working")
+        iterate_till_convergence(function)
 
 
 def iterate_till_convergence(function, estimate=1, tolerance=1e-8, max_iter=10000):
     p = estimate
     error = float('inf')
     for i in range(max_iter):
+        # if the series has converged return the value and number of iterations
         if error < tolerance:
             print(f'converged to {p_new} after {i} iterations')
-            return i
-        p_new = function(p)
+            return (p, i)
+
+        # if the value goes out of numeric range, series not converging
+        try:
+            p_new = function(p)
+        except OverflowError:
+            print(
+                f"Couldn't reach convergence. Value out of numeric range at p{i}")
+            return None
+
+        # metric for checking convergence
         error = abs(p_new - p)
         p = p_new
+
+    # if convergence couldn't be reached after specified maximum iterations
     raise Exception(
         f"Couldn't reach convergence even after {max_iter} iterations")
 

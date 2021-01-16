45cd9440d8bb3a8fe81d985c2c231aa8877468d6
adithyan-kv <adithyankv007@gmail.com>
2021-01-16 20:08:48 +0530

    Wrote code for plotting objective function
     -A 3d plot of the objective function is now being made
     -Have to annotate the path the MSD takes

--- assignment_2/prob1.py
+++ assignment_2/prob1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     print(x_final)
     plt.plot(errors)
     plt.show()
+    plot_objective_function(A, b)
 
 
 def solve_msd(A, x, b, error_threshold):
@@ -21,12 +23,15 @@ def solve_msd(A, x, b, error_threshold):
     errors = []
 
     for i in range(max_iterations):
+        # if the error is less than threshold return results
         if delta[0, 0] < error_threshold:
             print(f'Converged in {i} iterations')
             return(x, errors)
         q = np.matmul(A, r)
         alpha = delta / np.matmul(np.transpose(r), q)
         x = x + alpha * r
+
+        # recompute the residual every 50 iterations to avoid numerical error
         if i % 50 == 0:
             r = b - np.matmul(A, x)
         else:
@@ -34,6 +39,31 @@ def solve_msd(A, x, b, error_threshold):
         delta = np.matmul(np.transpose(r), r)
         errors.append(delta[0, 0])
     raise Exception("Maximum iterations exceeded with no convergence")
+
+
+def plot_objective_function(A, b):
+    x = np.arange(-10, 10, 0.1)
+    y = np.arange(-10, 10, 0.1)
+    X, Y = np.meshgrid(x, y)
+    f_values = np.zeros((len(x), len(y)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            x_vec = np.array([x[i], y[j]])
+            f = get_objective_function_value(A, b, x_vec)
+            # print(f)
+            f_values[i, j] = f
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    surface = ax.plot_surface(X, Y, f_values, cmap='rainbow')
+    fig.colorbar(surface, shrink=0.5, aspect=10)
+    plt.show()
+
+
+def get_objective_function_value(A, b, x):
+    term_1 = np.matmul(np.transpose(x), np.matmul(A, x))
+    term_2 = np.matmul(np.transpose(x), x)
+    f = 1 / 2 * term_1 + term_2
+    return f
 
 
 if __name__ == "__main__":
